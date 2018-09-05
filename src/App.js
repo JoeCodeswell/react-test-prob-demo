39222cd3632cec2d12fe723e0726b3a9e15a501b
Joe Dorocak <joecodeswell@gmail.com>
2018-09-05 15:28:02 -0700

    ready for github

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { testlist, testobj, testobjlist } from './m/data.mjs';
+
+const tlist = testlist;
+const tobj = testobj;
+const tobjlist = testobjlist;
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      mylist: tlist,
+      myobj: tobj,
+      myobjlist: tobjlist,
+      
+    }
+  }
   render() {
+    const {mylist, myobj, myobjlist} = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +30,24 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div class="content" style={{marginLeft: 2 + 'em', textAlign: 'left'}}>
+          <h2>testlist</h2>
+            <pre>
+            {"'mylist: '+JSON.stringify(mylist)"}<br/> 
+            {'mylist: '+JSON.stringify(mylist)}<br/>
+            </pre>
+          <h2>testobj</h2>
+            <pre>
+            {"'myobj: '+JSON.stringify(myobj)"}<br/> 
+            {'myobj: '+JSON.stringify(myobj)}<br/>              
+            </pre>
+          <h2>testobjlist</h2>
+            <pre>
+            {"'myobjlist: '+JSON.stringify(myobjlist)"}<br/> 
+            {'myobjlist: '+JSON.stringify(myobjlist)}<br/>                            
+            </pre>
+
+        </div>
       </div>
     );
   }
---
+++ src/m/data.mjs
@@ -0,0 +1,7 @@
+export const testlist =  ["one","two","three"];
+export const testobj =  {"k1":"v1", "k2":"v2", "k3":"v3"};
+export const testobjlist =  [
+    {"k1":"v1", "k2":"v2", "k3":"v3"},
+    {"k1":"v1", "k2":"v2", "k3":"v3"},
+    {"k4":"v4", "k2":"v2", "k3":"v3"}
+];
